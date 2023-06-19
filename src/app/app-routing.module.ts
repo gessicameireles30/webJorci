2983cd9402eccedbf60c9e7e0e1fd15555db337c
gessicameireles30 <gessicadasilvameireles@gmail.com>
2023-06-19 00:24:59 -0300

    angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { HomeComponent } from './components/home/home.component';
 import { PontosTuristicosComponent } from './components/pontos-turisticos/pontos-turisticos.component';
 import { ServicosComponent } from './components/servicos/servicos.component';
 import { ComidasComponent } from './components/comidas/comidas.component';
+import { CardComponent } from './components/card/card.component';
 
 const routes: Routes = [
   {path:'', component:HomeComponent},
   {path:'inicio', component:HomeComponent},
   {path:'pontos-turisticos', component:PontosTuristicosComponent},
   {path:'servicos', component:ServicosComponent},
-  {path: 'comidas', component:ComidasComponent}
+  {path: 'comidas', component:ComidasComponent},
+  {path: 'card', component:CardComponent}
 ];
 
 @NgModule({
